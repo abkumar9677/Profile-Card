f36ab2d3a8d945eb6463348a6d8658035517456c
abhi967792@gmail.com <abhi967792@gmail.com>
2020-12-26 23:09:02 +0530

    some features added

--- src/App.js
+++ src/App.js
@@ -1,10 +1,42 @@
 import './App.css';
-
+import pic from './profilepic.jpg'
+// import React from 'react';
+// import ReactDOM from 'react-dom';
+// const element = <img src="/src/profilepic.jpg" alt="ProfilePic" />;
+// ReactDOM.render(element,document.getElementById('photo'));
 function App() {
   return (
-    <div>
-      <h1>Hello, It's Abhishek Kumar.</h1>
-    </div>
+    <>
+      <div className="prof">
+        <h1 style={{color: 'white'}}>Profile Card</h1>
+      </div>
+        {/* <h1 className="txt">Hello, It's Abhishek Kumar.</h1> */}
+      <div className="profilediv">
+        <div className="pic">
+        <img src={pic} alt="profilepic" />
+        </div>
+
+        <div style={{textAlign: 'center'}}>
+          <span>P</span> 
+          <span>R</span> 
+          <span>O</span> 
+          <span>G</span> 
+          <span>R</span>
+          <span>A</span>
+          <span>M</span>
+          <span>M</span>
+          <span>E</span>
+          <span>R</span> 
+        </div>
+        <a href="https://github.com/abkumar9677">
+        <i className="fa fa-github-alt fa-5x" style={{textAlign:'center'}}></i>
+        </a>
+
+
+
+      </div>  
+           
+    </>
   );
 }
 
