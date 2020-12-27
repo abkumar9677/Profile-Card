249afac9899d78f711c6329fb1e2c6c4625660a7
abhi967792@gmail.com <abhi967792@gmail.com>
2020-12-27 22:33:59 +0530

    HTML code converted to react full code

--- src/App.js
+++ src/App.js
@@ -1,43 +1,65 @@
 import './App.css';
-import pic from './profilepic.jpg'
-// import React from 'react';
-// import ReactDOM from 'react-dom';
+import pic from './profilepic.jpg';
+import React from 'react';
+//import ReactDOM from 'react-dom';
 // const element = <img src="/src/profilepic.jpg" alt="ProfilePic" />;
 // ReactDOM.render(element,document.getElementById('photo'));
 function App() {
-  return (
-    <>
-      <div className="prof">
-        <h1 style={{color: 'white'}}>Profile Card</h1>
-      </div>
-        {/* <h1 className="txt">Hello, It's Abhishek Kumar.</h1> */}
-      <div className="profilediv">
-        <div className="pic">
-        <img src={pic} alt="profilepic" />
-        </div>
-
-        <div style={{textAlign: 'center'}}>
-          <span>P</span> 
-          <span>R</span> 
-          <span>O</span> 
-          <span>G</span> 
-          <span>R</span>
-          <span>A</span>
-          <span>M</span>
-          <span>M</span>
-          <span>E</span>
-          <span>R</span> 
-        </div>
-        <a href="https://github.com/abkumar9677">
-        <i className="fa fa-github-alt fa-5x" style={{textAlign:'center'}}></i>
-        </a>
-
-
-
-      </div>  
-           
-    </>
+  return React.createElement(
+    "div",
+    null,
+    React.createElement(
+      "div",
+      {
+        className: "prof"
+      },
+       React.createElement(
+        "h1",
+        {
+          style: {
+            color: "white"
+          }
+        },
+        "Profile Card"
+      )
+    ),
+    React.createElement(
+      "div",
+      {
+        className: "profilediv"
+      },
+      React.createElement(
+        "div",
+        {
+          className: "pic"
+        },
+        React.createElement("img", {
+          src: pic,
+          alt: "profilepic"
+        })
+      ),
+      React.createElement(
+        "div",
+        {
+          style: {
+            textAlign: "center",
+            marginTop: "50px"
+          }
+        },
+        React.createElement("span", null, "P"),
+        React.createElement("span", null, "R"),
+        React.createElement("span", null, "O"),
+        React.createElement("span", null, "G"),
+        React.createElement("span", null, "R"),
+        React.createElement("span", null, "A"),
+        React.createElement("span", null, "M"),
+        React.createElement("span", null, "M"),
+        React.createElement("span", null, "E"),
+        React.createElement("span", null, "R")
+      )
+    )
   );
+
 }
 
 export default App;
